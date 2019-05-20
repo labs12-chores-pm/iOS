52ab9d0a62a004c4aecf38fe412baf8b32288f12
Jerrick Warren <Jerrick.Warren@gmail.com>
2019-05-20 11:33:12 -0700

    added " AddTaskVC and AddTaskStoryboard" ,  Connected outlets.

---
+++ ShoppingList/ShoppingList/View Controllers/AddTaskViewController.swift
@@ -0,0 +1,42 @@
+//
+//  AddTaskViewController.swift
+//  ShoppingList
+//
+//  Created by Jerrick Warren on 5/20/19.
+//  Copyright © 2019 Lambda School Labs. All rights reserved.
+//
+
+import UIKit
+
+class AddTaskViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    
+    
+    
+    
+    // IBOutlets
+    
+    @IBAction func categoryTextField(_ sender: Any) {
+    }
+    @IBAction func taskTextField(_ sender: Any) {
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
