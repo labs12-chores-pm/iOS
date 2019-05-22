ac63add1c8759d5729056f4bab7d570d7590b2c6
nateyoungren <nathanaelyoungren@gmail.com>
2019-05-22 12:44:07 -0700

    Clean AddTaskViewController

--- ShoppingList/ShoppingList/View Controllers/AddTaskViewController.swift
+++ ShoppingList/ShoppingList/View Controllers/AddTaskViewController.swift
@@ -8,41 +8,7 @@
 
 import UIKit
 
-class AddTaskViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UITextFieldDelegate {
-   
-    // Properties
-    var categories: [Category]?
-    var userController: UserController?
-    var currentUser: User?
-    var householdController: HouseholdController?
-    var taskController : TaskController?
-    var categoryController : CategoryController?
-    
-    var task : Task?
-    var catID : UUID?
-    var category : Category?
-    var household : Household?
-    
-    let mainCategoriesViewController = MainCategoriesViewController()
-    
-    // category reuse Identifier
-    
-    let reuseIdentifier = "searchCategoryCell"
-    
-    var filteredResults : [Category] = [] {
-        didSet {
-            DispatchQueue.main.async {
-                self.categoryTableView.reloadData()
-            }
-        }
-    }
-    
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        
-        fetchCategories()
-    }
-    
+class AddTaskViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -59,8 +25,6 @@ class AddTaskViewController: UIViewController, UITableViewDataSource, UITableVie
             self.currentUser = tabBar.currentUser
             self.householdController = tabBar.householdController
             self.userController = tabBar.userController
-            
-            
         }
         
         if let householdController = householdController, let user = currentUser {
@@ -72,13 +36,11 @@ class AddTaskViewController: UIViewController, UITableViewDataSource, UITableVie
                 self.household = household
             }
         }
+    }
     
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
         fetchCategories()
-        print(categories)
-        
-        
-        
-        
     }
     
     @objc func fetchCategories() {
@@ -94,9 +56,8 @@ class AddTaskViewController: UIViewController, UITableViewDataSource, UITableVie
         }
     }
     
-    
     func checkCategory() {
-        // check inside the filtered category
+
         if let category = filteredResults.first {
             self.catID = category.identifier
             self.category = category
@@ -104,22 +65,8 @@ class AddTaskViewController: UIViewController, UITableViewDataSource, UITableVie
             guard let user = currentUser,
                   let name = addCategoryTextField.text else { return }
             let householdId = user.currentHouseholdId
-           
             self.category = categoryController?.createCategory(householdId: householdId, name: name)
         }
-        
-        
-    }
-    
-    // IBOutlets
-    
-    @IBOutlet weak var categoryTableView: UITableView!
-    
-    @IBOutlet weak var addCategoryTextField: UITextField!
-   
-    @IBOutlet weak var addTaskTextField: UITextField!
-    
-    @IBAction func tappedCategoryTextField(_ sender: Any) {
     }
     
     @IBAction func addTaskButton(_ sender: Any) {
@@ -133,60 +80,6 @@ class AddTaskViewController: UIViewController, UITableViewDataSource, UITableVie
         self.task = taskController?.createTask(description: addTask, categoryId: categoryID, assineeIds: [], dueDate: Date(), isComplete: false)
         
        performSegue(withIdentifier: "back2task", sender: self)
-        
-    
-    }
-    
-    
-    //  add the the TextField
-    
-    
-    
-    // MARK: UITextFieldDelegate
-    func textFieldDidEndEditing(_ textField: UITextField) {
-    
-        print("What whould you like to go here?")
-        
-    }
-    
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-    textField.resignFirstResponder()
-    return true
-    }
-    
-    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-        
-        guard let text = addCategoryTextField.text, let categories = categories else { return true }
-        let filteredResults = categories.filter{$0.name.lowercased().contains(text.lowercased())}
-        //let filteredNames = filteredResults.compactMap { $0.name }
-        
-        self.filteredResults = filteredResults
-        
-        return true
-    }
-    
-    
-    // Tableview protocols
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return filteredResults.count
-    }
-    
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-         let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath)
-        print(cell)
-        cell.textLabel?.text = filteredResults[indexPath.row].name
-        
-        return cell
-    }
-    
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        // Row selected, so set textField to relevant value, hide tableView
-        // endEditing can trigger some other action according to requirements
-        addCategoryTextField.text = filteredResults[indexPath.row].name
-        categoryTableView.isHidden = true
-        addCategoryTextField.endEditing(true)
     }
     
     // perform segue
@@ -200,12 +93,6 @@ class AddTaskViewController: UIViewController, UITableViewDataSource, UITableVie
                   let household = household,
             let userController = userController else {return}
             
-            
-           
-            print(task)
-            print(category)
-            
-        
             destinationVC.taskController = taskController
             destinationVC.task = task
             destinationVC.category = category
@@ -216,10 +103,78 @@ class AddTaskViewController: UIViewController, UITableViewDataSource, UITableVie
         
     }
     
+    // IBOutlets
+    
+    @IBOutlet weak var categoryTableView: UITableView!
+    @IBOutlet weak var addCategoryTextField: UITextField!
+    @IBOutlet weak var addTaskTextField: UITextField!
+    @IBAction func tappedCategoryTextField(_ sender: Any) {
+    }
+    
+    // Properties
+    var categories: [Category]?
+    var userController: UserController?
+    var currentUser: User?
+    var householdController: HouseholdController?
+    var taskController : TaskController?
+    var categoryController : CategoryController?
+    
+    var task : Task?
+    var catID : UUID?
+    var category : Category?
+    var household : Household?
+    
+    //    let mainCategoriesViewController = MainCategoriesViewController()
+    
+    let reuseIdentifier = "searchCategoryCell"
+    
+    var filteredResults : [Category] = [] {
+        didSet {
+            DispatchQueue.main.async {
+                self.categoryTableView.reloadData()
+            }
+        }
+    }
+}
+
+extension AddTaskViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return filteredResults.count
+    }
     
     
-    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-        return 0.0
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath)
+        print(cell)
+        cell.textLabel?.text = filteredResults[indexPath.row].name
+        
+        return cell
+    }
+    
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        addCategoryTextField.text = filteredResults[indexPath.row].name
+        categoryTableView.isHidden = true
+        addCategoryTextField.endEditing(true)
+    }
+}
+
+extension AddTaskViewController: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        
+        guard let text = addCategoryTextField.text, let categories = categories else { return true }
+        let filteredResults = categories.filter{$0.name.lowercased().contains(text.lowercased())}
+        
+        self.filteredResults = filteredResults
+        
+        return true
     }
 }
 
