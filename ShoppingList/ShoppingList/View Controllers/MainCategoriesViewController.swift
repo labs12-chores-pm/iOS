271839a8bcbc01e158893732eb690709e8d123cc
nateyoungren <nathanaelyoungren@gmail.com>
2019-05-30 11:46:46 -0700

    Added my to-do's list to main view.

--- ShoppingList/ShoppingList/View Controllers/MainCategoriesViewController.swift
+++ ShoppingList/ShoppingList/View Controllers/MainCategoriesViewController.swift
@@ -15,6 +15,8 @@ class MainCategoriesViewController: UIViewController {
         super.viewDidLoad()
         categoriesTableView.dataSource = self
         categoriesTableView.delegate = self
+        myToDosTableView.dataSource = self
+        myToDosTableView.delegate = self
         
         if let tabBar = self.tabBarController as? TabViewViewController {
             
@@ -70,6 +72,30 @@ class MainCategoriesViewController: UIViewController {
         }
     }
     
+    private func fetchToDos() {
+        guard let household = household, let currentUser = currentUser, let taskController = taskController else { return }
+        
+        taskController.fetchTasks(userId: currentUser.identifier) { (tasks, error) in
+            if let error = error {
+                print(error)
+                return
+            }
+            
+            if let tasks = tasks {
+                let householdTasks = tasks.filter({ $0.householdId == household.identifier })
+                let incompleteTasks = householdTasks.filter({ $0.isComplete == false })
+                
+                self.myTasks = incompleteTasks.sorted(by: { (task1, task2) -> Bool in
+                    task1.dueDate > task2.dueDate
+                })
+            }
+            
+            DispatchQueue.main.async {
+                self.myToDosTableView.reloadData()
+            }
+        }
+    }
+    
     private func updateViews() {
         guard let household = household, let user = currentUser else { return }
         
@@ -92,6 +118,24 @@ class MainCategoriesViewController: UIViewController {
             guard let destinationVC = segue.destination as? AddCategoryViewController, let user = currentUser else { return }
             destinationVC.categoryController = categoryController
             destinationVC.currentUser = user
+        }
+        
+        if segue.identifier == "ShowToDo" {
+            guard let destinationVC = segue.destination as? TaskViewController,
+            let index = myToDosTableView.indexPathForSelectedRow,
+            let tasks = myTasks, let household = household, let userController = userController, let categories = categories,
+            let user = currentUser, let notesController = notesController, let notificationHelper = notificationHelper else { return }
+            let task = tasks[index.row]
+            let category = categories.filter { $0.identifier == task.categoryId }.first!
+            
+            destinationVC.taskController = taskController
+            destinationVC.task = task
+            destinationVC.category = category
+            destinationVC.household = household
+            destinationVC.userController = userController
+            destinationVC.currentUser = user
+            destinationVC.notesController = notesController
+            destinationVC.notificationHelper = notificationHelper
         }
         
         if segue.identifier == "ShowTasks" {
@@ -121,6 +165,8 @@ class MainCategoriesViewController: UIViewController {
     @IBOutlet weak var addCategoryButton: UIButton!
     @IBOutlet weak var categoriesLabel: UILabel!
     @IBOutlet weak var categoriesTableView: UITableView!
+    @IBOutlet weak var myToDosTableView: UITableView!
+    @IBOutlet weak var myToDosLabel: UILabel!
     
     var taskController: TaskController?
     var userController: UserController?
@@ -129,7 +175,12 @@ class MainCategoriesViewController: UIViewController {
     var categoryController: CategoryController?
     var notesController: NotesController?
     var categories: [Category]?
-    var household: Household?
+    var household: Household? {
+        didSet {
+            fetchToDos()
+        }
+    }
+    var myTasks: [Task]?
     var notificationHelper: NotificationHelper?
     
 }
@@ -137,19 +188,49 @@ class MainCategoriesViewController: UIViewController {
 extension MainCategoriesViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return categories?.count ?? 0
+        
+        if tableView == categoriesTableView {
+            return categories?.count ?? 0
+        }
+        
+        if tableView == myToDosTableView {
+            return myTasks?.count ?? 0
+        }
+        
+        return 0
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath)
         
-        if let categories = categories {
-            cell.textLabel?.text = categories[indexPath.row].name
+        var cell = UITableViewCell()
+        
+        if tableView == categoriesTableView {
+            
+            cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath)
+            
+            if let categories = categories {
+                cell.textLabel?.text = categories[indexPath.row].name
+            }
         }
+        
+        if tableView == myToDosTableView {
+            
+            cell = tableView.dequeueReusableCell(withIdentifier: "MyToDosCell", for: indexPath)
+            
+            if let tasks = myTasks {
+                let task = tasks[indexPath.row]
+                cell.textLabel?.text = task.description
+                cell.detailTextLabel?.text = task.dueDate.string(style: .short, showTime: true)
+            }
+        }
+        
         return cell
     }
     
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        
+        if tableView != categoriesTableView { return }
+        
         if editingStyle == .delete {
             guard let categories = categories, let categoryController = categoryController else { return }
             
